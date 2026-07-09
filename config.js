a4084864741e09faec71ac1d167445e04dcfb5ea
pacserjavier <pacserjavier@gmail.com>
2026-07-09 04:03:53 -0500

    proyecto listo para el desplegue V2

---
+++ config.js
@@ -0,0 +1,11 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+export const PORT = process.env.PORT || 3000
+export const PORT = process.env.DATABASE_HOST || localhost
+export const PORT = process.env.DATABASE_USER || root
+export const PORT = process.env.DATABASE_PASSWORD
+export const PORT = process.env.DATABASE || bd_ventas
+export const PORT = process.env.DATABASE_PORT || 3306
+
+
--- db.js
+++ db.js
@@ -3,7 +3,8 @@ const conn = mysql.createConnection({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE
+    database: process.env.DATABASE,
+    port: process.env.DATABASE_PORT
 });
 
 conn.connect((err) => {
