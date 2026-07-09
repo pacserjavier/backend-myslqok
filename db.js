ee7f187c7e66d4fb6d917074dff6e450e7152d6d
pacserjavier <pacserjavier@gmail.com>
2026-07-09 05:40:56 -0500

    proyecto listo para el desplegue OK

--- db.js
+++ db.js
@@ -1,10 +1,5 @@
 const mysql = require("mysql2");
 const {DATABASE_PORT, DATABASE_HOST, DATABASE_USER,  DATABASE_PASSWORD, DATABASE} = require("./config");
-console.log("HOST:", process.env.DATABASE_HOST);
-console.log("USER:", process.env.DATABASE_USER);
-console.log("PASSWORD:", process.env.DATABASE_PASSWORD);
-console.log("DB:", process.env.DATABASE);
-console.log("PORT:", process.env.DATABASE_PORT);
 
 const conn = mysql.createConnection({
     host: DATABASE_HOST,
