c02c8b4b81f4e6a99161d8e7193572c4d677519f
pacserjavier <pacserjavier@gmail.com>
2026-07-09 04:24:15 -0500

    proyecto listo para el desplegue OK

--- config.js
+++ config.js
@@ -2,14 +2,15 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const PORT = process.env.PORT || 3000
-const DATABASE_HOST = process.env.DATABASE_HOST || localhost
-const DATABASE_USER = process.env.DATABASE_USER || root
-const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD
-const DATABASE = process.env.DATABASE || bd_ventas
+const DATABASE_HOST = process.env.DATABASE_HOST || 'localhost'
+const DATABASE_USER = process.env.DATABASE_USER || 'root'
+const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD || ''
+const DATABASE = process.env.DATABASE || 'bd_ventas'
 const DATABASE_PORT = process.env.DATABASE_PORT || 3306
 
 
-module.exports = {PORT,
+module.exports = {
+    PORT,
     DATABASE_HOST,
     DATABASE_USER,
     DATABASE_PASSWORD,
